f8202ff7c34462df1dc548ab66f180b29c5b15de
Joseph <chakkourjoseph@gmail.com>
2019-07-26 22:34:46 +0400

    2-varables.py

--- 2-variables.py
+++ 2-variables.py
@@ -1,8 +1,10 @@
-x = input()
-y = input()
+x = input(2)
+y = input(5)
 
-#your code goes here
+temp = x
+x = y
+y = temp
 
 print("I swapped x and y, look:")
-print("x is now ",x)
-print("y is now ",y)
+print("x is now ",y)
+print("y is now ",x)
